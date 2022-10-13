1dc79af4f9c5ff91f2828a393f29fb61514606e3
RexShack <rexshackuk@gmail.com>
2022-10-13 17:13:59 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,6 +2,7 @@ fx_version "adamant"
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 game "rdr3"
 
-description 'Rexshack Gaming : IPLs'
+author 'rexshack'
+description 'redm-ipls'
 
 client_script 'client.lua'
